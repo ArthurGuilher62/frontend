5b06830a0d5c5817a14d53bfabe4c84cf0341b0b
TiagoTeixeira-2005 <jorgelucass1984@gmail.com>
2024-12-07 08:22:01 -0300

    Modificação no Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './assets/logo.png';
+import logo from './assets/logo.png'; 
 import unb from './assets/unb.png';
 import './App.css';
 
@@ -7,44 +7,16 @@ function App() {
     <div className="App">
       <img src={unb} className="unb-logo" alt="unb" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Gastos Publicos</h1>
-        <a
-          className="gastos"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          gastos
-        </a>
-        <a
-          className="monitoramento"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          monitoramento
-        </a>
-        <a
-          className="sobreOprojeto"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          sobre o projeto
-        </a>
-        <a
-          className="sobreNos"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          sobre nós
-        </a>
+        <div className="logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div id="title">Gastos Públicos</div>
+        </div>
+        <ul className="navigation">
+          <li><a className="navigation__link" href="" target="_blank" rel="noopener noreferrer">Gastos</a></li>
+          <li><a className="navigation__link" href="" target="_blank" rel="noopener noreferrer">Monitoramento</a></li>
+          <li><a className="navigation__link" href="" target="_blank" rel="noopener noreferrer">Sobre o projeto</a></li>
+          <li><a className="navigation__link" href="" target="_blank" rel="noopener noreferrer">Sobre nós</a></li>
+        </ul>
       </header>
     </div>
   );
